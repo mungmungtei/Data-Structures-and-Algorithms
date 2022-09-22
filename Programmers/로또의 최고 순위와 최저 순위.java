30dc4861b29b7224f228c4ffb5a611993f96d52f
mungmungtei <106861237+mungmungtei@users.noreply.github.com>
2022-09-22 15:32:18 +0900

    Update 로또의 최고 순위와 최저 순위.java

--- Programmers/로또의 최고 순위와 최저 순위.java
+++ Programmers/로또의 최고 순위와 최저 순위.java
@@ -1,4 +1,4 @@
-<https://school.programmers.co.kr/learn/courses/30/lessons/77484>
+[문제](https://school.programmers.co.kr/learn/courses/30/lessons/77484)
 
 class Solution {
     public int[] solution(int[] lottos, int[] win_nums) {
